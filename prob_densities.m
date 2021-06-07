1e3f707bf3d80cd895e994b3896bf8c950ace3c7
jwid8259 <63948487+jwid8259@users.noreply.github.com>
2021-06-07 23:08:09 +1000

    Create prob_densities.m

---
+++ prob_densities.m
@@ -0,0 +1,13 @@
+function h = prob_densities(xy)
+% prob_densities.m
+% Generate 3D probability density distributions for input (x,y)
+% electrode position data.
+% xy = input array of size N x 2, where N is the number of positions
+% h = output 19 x 19 probability density array in x,y coords
+
+    hist = hist3(xy,'Ctrs',{1:0.5:10 1:0.5:10},'CdataMode','auto');
+    normhist = hist/sum(hist(:)); % probability normalisation
+    h = reshape(normhist, [19*19, 1]);
+    h = h'; % transpose for correct positions when plotting
+    
+end
