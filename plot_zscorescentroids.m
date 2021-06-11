bc6b5735c23ac2bf3480a5057487a5a4143e79e7
jwid8259 <63948487+jwid8259@users.noreply.github.com>
2021-06-11 15:41:35 +1000

    Update plot_zscorescentroids.m

--- plot_zscorescentroids.m
+++ plot_zscorescentroids.m
@@ -10,13 +10,15 @@ load('somePatt_zscores_d1.mat');
 t = 300; % Time after stimulus onset
 tt = t+10; % Second time tt > t, same detected pattern
 pos = xy_somePatt_d1(t_somePatt_d1 == t | t_somePatt_d1 == tt,:);
-u = 8/9; % electrode spacing for axes
+u = 410; % electrode spacing for axes;
+% the extra 10 is due to some weird cuts of data points due to hists
+pos = pos*u-200;
 
 % colormap jet
 subplot(121)
 amps = zscores_1301;
 ampssmooth = interp2(amps,2); % smooth by interpolation
-imagesc(1:10,1:10,ampssmooth)
+imagesc(1:4000,1:4000,ampssmooth)
 hold on
 plot(pos(1,1),pos(1,2),'r.','MarkerSize',40)
 % plot([1 2],[1 2],'r')
@@ -25,8 +27,8 @@ plot(pos(1,1),pos(1,2),'r.','MarkerSize',40)
 c = colorbar;
 caxis([-1,4])
 
-set(gca,'XTick',1.5:u:9.5, 'XTickLabel', 1:10)
-set(gca,'YTick',1.5:u:9.5, 'YTickLabel', 1:10)
+set(gca,'XTick',0:1000:4000)
+set(gca,'YTick',0:1000:4000)
 set(gca,'YDir','normal')
 
 set(gca,'FontSize',20)
@@ -39,7 +41,7 @@ axis square
 subplot(122)
 amps = zscores_1311;
 ampssmooth = interp2(amps,2); % smooth by interpolation
-imagesc(1:10,1:10,ampssmooth)
+imagesc(1:4000,1:4000,ampssmooth)
 hold on
 plot(pos(1,1),pos(1,2),'ro','MarkerSize',10,'LineWidth',2)
 plot(pos(2,1),pos(2,2),'r.','MarkerSize',40)
@@ -47,8 +49,8 @@ plot(pos(2,1),pos(2,2),'r.','MarkerSize',40)
 c = colorbar;
 caxis([-1,4])
 
-set(gca,'XTick',1.5:u:9.5, 'XTickLabel', 1:10)
-set(gca,'YTick',1.5:u:9.5, 'YTickLabel', 1:10)
+set(gca,'XTick',0:1000:4000)
+set(gca,'YTick',0:1000:4000)
 set(gca,'YDir','normal')
 
 set(gca,'FontSize',20)
